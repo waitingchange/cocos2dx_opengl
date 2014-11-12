895a68877099f4be5a95bf1934e039893680c360
jiangmin <jminht@163.com>
2014-11-12 11:26:47 +0800

    using namespace cocos2d, othersize class in cocos2d would be undefined

--- MyCppGame/Classes/HelloWorldScene.h
+++ MyCppGame/Classes/HelloWorldScene.h
@@ -2,6 +2,7 @@
 #define __HELLOWORLD_SCENE_H__
 
 #include "cocos2d.h"
+USING_NS_CC;
 
 class HelloWorld : public cocos2d::Layer
 {
@@ -17,6 +18,11 @@ public:
     
     // implement the "static create()" method manually
     CREATE_FUNC(HelloWorld);
+
+	virtual void visit(Renderer *renderer, const Mat4 &transform, bool transformUpdated) override;
+	void onDraw();
+private:
+	CustomCommand _command;
 };
 
 #endif // __HELLOWORLD_SCENE_H__
